df24b70bff5dd6c57645733c17da5e28de408911
Tsingper <tsingper@outlook.com>
2023-12-16 22:13:36 +0800

    重构redis代码

--- redis/index.js
+++ redis/index.js
@@ -1,81 +1,76 @@
 const redis = require('redis');
 
 /**
- * @description 云函数入口函数
+ * 云函数入口函数
  * @param {Object} params - 参数对象
  * @param {string} params.action - 操作类型（select、insert、update、delete)
- * @param {string} params.msg - 操作信息(查询：无，插入：插入的数据，更新：更新的数据，删除：删除的条件)
+ * @param {string} params.key - Redis键
+ * @param {string} [params.value] - 操作值 (对于 insert 和 update 操作)
  * @returns {Promise} 返回云函数执行结果
  */
-function main(params) {
-  return new Promise((resolve, reject) => {
+async function main(params) {
     const client = redis.createClient({
-      host: 'localhost',
-      port: '6379',
+        host: 'localhost',
+        port: '6379',
     });
 
     client.on('error', (error) => {
-      reject({ error: error.message });
+        console.error(error);
+        throw new Error(error.message);
     });
 
-    let query = '';
-    let queryValues = [];
-    switch (params.action) {
-      case 'select':
-        query = 'SELECT * FROM test'; // 示例查询
-        break;
-      case 'insert':
-        query = `INSERT INTO test(data) VALUES ('${params.msg}')`; // 示例插入
-        break;
-      case 'update':
-        queryValues = [params.msg];
-        query = `UPDATE test SET ?`; // 示例更新
-        break;
-      case 'delete':
-        query = `DELETE FROM test WHERE ${params.msg}`; // 示例删除
-        break;
+    try {
+        switch (params.action) {
+            case 'select':
+                // 使用 GET 命令读取键值
+                return await getRedisValue(client, params.key);
+            case 'insert':
+            case 'update':
+                // 使用 SET 命令设置键值
+                return await setRedisValue(client, params.key, params.value);
+            case 'delete':
+                // 使用 DEL 命令删除键
+                return await deleteRedisKey(client, params.key);
+            default:
+                throw new Error('Unsupported action type');
+        }
+    } finally {
+        client.quit();
     }
-
-    // Perform Redis operations based on the action
-    switch (params.action) {
-      case 'select':
-        client.get('test', (error, result) => {
-          if (error) {
-            reject({ error: error.message });
-          } else {
-            resolve({ results: result });
-          }
-        });
-        break;
-      case 'insert':
-        client.set('test', params.msg, (error, result) => {
-          if (error) {
-            reject({ error: error.message });
-          } else {
-            resolve({ results: result });
-          }
-        });
-        break;
-      case 'update':
-        client.set('test', params.msg, (error, result) => {
-          if (error) {
-            reject({ error: error.message });
-          } else {
-            resolve({ results: result });
-          }
-        });
-        break;
-      case 'delete':
-        client.del('test', (error, result) => {
-          if (error) {
-            reject({ error: error.message });
-          } else {
-            resolve({ results: result });
-          }
-        });
-        break;
-    }
-  });
 }
 
-exports.main = main;
+function getRedisValue(client, key) {
+    return new Promise((resolve, reject) => {
+        client.get(key, (error, result) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve({ result });
+            }
+        });
+    });
+}
+
+function setRedisValue(client, key, value) {
+    return new Promise((resolve, reject) => {
+        client.set(key, value, (error, result) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve({ result });
+            }
+        });
+    });
+}
+
+function deleteRedisKey(client, key) {
+    return new Promise((resolve, reject) => {
+        client.del(key, (error, result) => {
+            if (error) {
+                reject(error);
+            } else {
+                resolve({ result });
+            }
+        });
+      })
+}
